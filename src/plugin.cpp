b5ab7af4dff4215b74538fea9a34de24cfc514ce
Rai <56082069+raicool@users.noreply.github.com>
2025-10-27 19:41:41 -0400

    removed using namespace & update info display

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -11,11 +11,9 @@
 #include "struct.h"
 #include "util.h"
 
-
+//#include "mk7-memory/include/System/RootSystem.hpp"
 #include "sead/include/basis/seadTypes.h"
 #include "sead/include/math/seadVector.hpp"
-
-using namespace CTRPluginFramework;
 
 //
 // credits to https://twitter.com/B_squo
@@ -98,24 +96,24 @@ void render_info()
 	player_kmh = player->player_speed * 10.376756f;
 
 	// switch the OSD render screen whenever dpad down is pressed
-	if (Controller::IsKeyPressed(DPadDown))
+	if (CTRPluginFramework::Controller::IsKeyPressed(DPadDown))
 	{
 		screen_switch = !screen_switch;
 	}
 
-	const Screen screen = screen_switch ? OSD::GetBottomScreen() : OSD::GetTopScreen();
+	const CTRPluginFramework::Screen screen = screen_switch ? CTRPluginFramework::OSD::GetBottomScreen() : CTRPluginFramework::OSD::GetTopScreen();
 
 	if (player->miniturbo)
 	{
-		screen.Draw(Utils::Format("(+%f)", player->miniturbo - miniturbo_old), 10, 110, Color::Gray);
+		screen.Draw(CTRPluginFramework::Utils::Format("(+%f)", player->miniturbo - miniturbo_old), 10, 110, Color::Gray);
 
-		Color mt_text_color;
+		CTRPluginFramework::Color mt_text_color;
 
 		if (player->miniturbo < 220)
 		{
 			mt_text_color = Color::Gray;
 
-			screen.Draw(Utils::Format((player->miniturbo < 100) ? "MT :  %.2f/220.00" : "MT : %.2f/220.00", player->miniturbo), 10, 120, mt_text_color);
+			screen.Draw(CTRPluginFramework::Utils::Format((player->miniturbo < 100) ? "MT :  %.2f/220.00" : "MT : %.2f/220.00", player->miniturbo), 10, 120, mt_text_color);
 		}
 		else
 		{
@@ -131,39 +129,40 @@ void render_info()
 				mt_text_color = Color::Red;
 			}
 
-			screen.Draw(Utils::Format((player->miniturbo < 100) ? "MT :  %.2f/460.00" : "MT : %.2f/460.00", player->miniturbo), 10, 120, mt_text_color);
+			screen.Draw(CTRPluginFramework::Utils::Format((player->miniturbo < 100) ? "MT :  %.2f/460.00" : "MT : %.2f/460.00", player->miniturbo), 10, 120, mt_text_color);
 		}
 	}
 
-	screen.Draw(Utils::Format("%f km/h", player_kmh), 10, 130);
+	screen.Draw(CTRPluginFramework::Utils::Format("%f km/h", player_kmh), 10, 130);
 	//screen.Draw(Utils::Format("Yaw Int : %f\xb0", radian_to_degrees(player->yaw_strength_internal)), 10, 90, Color::DeepSkyBlue);
 	//screen.Draw(Utils::Format("Yaw Ext : %f\xb0", radian_to_degrees(player->yaw_strength_external)), 10, 80, Color::DodgerBlue);
-	screen.Draw(Utils::Format("YV Int : %f", player->random_float * 10.376756f), 10, 70, Color::Orange);
+	screen.Draw(CTRPluginFramework::Utils::Format("YV Int : %f", player->random_float * 10.376756f), 280, 210, CTRPluginFramework::Color::Orange);
+	screen.Draw(CTRPluginFramework::Utils::Format("XV Int : %f", player->random_float2 * 10.376756f), 280, 220, CTRPluginFramework::Color::SkyBlue);
 
 
 	kmh_delta = player_kmh - player_old_kmh;
 
 	if (kmh_delta == 0)
 	{
-		screen.Draw(Utils::Format("( %f)", kmh_delta), 10, 140, Color::Gray);
+		screen.Draw(CTRPluginFramework::Utils::Format("( %f)", kmh_delta), 10, 140, CTRPluginFramework::Color::Gray);
 	}
 	else if (kmh_delta >= 0)
 	{
-		screen.Draw(Utils::Format("(+%f)", kmh_delta), 10, 140, Color::ForestGreen);
+		screen.Draw(CTRPluginFramework::Utils::Format("(+%f)", kmh_delta), 10, 140, CTRPluginFramework::Color::ForestGreen);
 	}
 	else
 	{
-		screen.Draw(Utils::Format("(%f)", kmh_delta), 10, 140, Color::Maroon);
+		screen.Draw(CTRPluginFramework::Utils::Format("(%f)", kmh_delta), 10, 140, CTRPluginFramework::Color::Maroon);
 	}
 
 	//screen.Draw(Utils::Format("Trick %i", player->trick), 10, 150, Color::SkyBlue);
-	screen.Draw(Utils::Format("Frame %i", input_frame_count), 10, 160, Color::SkyBlue);
-	screen.Draw(Utils::Format("Air : %i", player->player_airtime), 10, 170, (player->player_airtime == 0) ? Color::Red : Color::LimeGreen);
-	screen.Draw(Utils::Format("Boost : %i", player->boost_duration), 10, 180, player->boosting ? Color::LimeGreen : Color::Red);
-	screen.Draw(Utils::Format("KCL : %s", kcl_type_name_from_char(player->ground_type_id)), 10, 190, (player->player_airtime == 0) ? Color::DeepSkyBlue : Color::Gray);
-	screen.Draw(Utils::Format("X : %.3f", player->player_x), 10, 200, Color::DodgerBlue);
-	screen.Draw(Utils::Format("Y : %.3f", player->player_y), 10, 210, Color::DodgerBlue);
-	screen.Draw(Utils::Format("Z : %.3f", player->player_z), 10, 220, Color::DodgerBlue);
+	screen.Draw(CTRPluginFramework::Utils::Format("Frame %i", input_frame_count), 10, 160, CTRPluginFramework::Color::SkyBlue);
+	screen.Draw(CTRPluginFramework::Utils::Format("Air : %i", player->player_airtime), 10, 170, (player->player_airtime == 0) ? CTRPluginFramework::Color::Red : CTRPluginFramework::Color::LimeGreen);
+	screen.Draw(CTRPluginFramework::Utils::Format("Boost : %i", player->boost_duration), 10, 180, player->boosting ? CTRPluginFramework::Color::LimeGreen : CTRPluginFramework::Color::Red);
+	screen.Draw(CTRPluginFramework::Utils::Format("KCL : %s", kcl_type_name_from_char(player->ground_type_id)), 10, 190, (player->player_airtime == 0) ? CTRPluginFramework::Color::DeepSkyBlue : CTRPluginFramework::Color::Gray);
+	screen.Draw(CTRPluginFramework::Utils::Format("X : %.3f", player->player_x), 10, 200, CTRPluginFramework::Color::DodgerBlue);
+	screen.Draw(CTRPluginFramework::Utils::Format("Y : %.3f", player->player_y), 10, 210, CTRPluginFramework::Color::DodgerBlue);
+	screen.Draw(CTRPluginFramework::Utils::Format("Z : %.3f", player->player_z), 10, 220, CTRPluginFramework::Color::DodgerBlue);
 	
 
 	miniturbo_old = player->miniturbo;
@@ -175,13 +174,13 @@ void render_info()
 	render_player_yaw(&screen, player);
 }
 
-void info(MenuEntry* entry)
+void info(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
- 		if (!System::IsCitra())
+ 		if (!CTRPluginFramework::System::IsCitra())
 		{
-			OSD::Notify("info is only available if using citra!", Color::White, Color::Maroon);
+			CTRPluginFramework::OSD::Notify("info is only available if using citra!", Color::White, Color::Maroon);
 			entry->Disable();
 			return;
 		}
@@ -191,18 +190,18 @@ void info(MenuEntry* entry)
 			0xE59400E4, 0xED9F9AC2, 0xE5901008, 0xE5911110, 0xE3110001, 0x03110010, 0xE59410FC
 		};
 
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
 
 		debug_hook.Initialize(func_ptr, reinterpret_cast<u32>(debug_hook_callback));
 
-		if (debug_hook.Enable() == HookResult::Success)
+		if (debug_hook.Enable() == CTRPluginFramework::HookResult::Success)
 		{
-			OSD::Notify("debug hook enabled!", Color::LimeGreen);
+			CTRPluginFramework::OSD::Notify("debug hook enabled!", Color::LimeGreen);
 //			OSD::Notify(Utils::Format("%p", debug_hook_callback), Color::Gray);
 		}
 		else
 		{
-			OSD::Notify("debug hook failed to enable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("debug hook failed to enable!", CTRPluginFramework::Color::Red);
 			debug_hook.Disable();
 			entry->Disable();
 		}
@@ -221,7 +220,7 @@ void info(MenuEntry* entry)
 	}
 }
 
-void replay_ghost(MenuEntry* entry)
+void replay_ghost(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
@@ -231,18 +230,18 @@ void replay_ghost(MenuEntry* entry)
 			0xe590002c, 0xe7900101, 0xe1a01002, 0xe590002c, 0xe1a00000, 0xe58010e0, 0xe5911038, 0xe58010e4
 		};
 
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
 
 		live_replay_hook.Initialize(func_ptr, reinterpret_cast<u32>(live_replay));
 		live_replay_hook.SetFlags(USE_LR_TO_RETURN | EXECUTE_OI_AFTER_CB);
 
-		if (live_replay_hook.Enable() == HookResult::Success)
+		if (live_replay_hook.Enable() == CTRPluginFramework::HookResult::Success)
 		{
-			OSD::Notify("live_replay hook enabled!", Color::LimeGreen);
+			CTRPluginFramework::OSD::Notify("live_replay hook enabled!", Color::LimeGreen);
 		}
 		else
 		{
-			OSD::Notify("live_replay hook failed to enable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("live_replay hook failed to enable!", Color::Red);
 			live_replay_hook.Disable();
 			entry->Disable();
 		}
@@ -252,11 +251,11 @@ void replay_ghost(MenuEntry* entry)
 	{
 		if (live_replay_hook.Disable() == HookResult::Success)
 		{
-			OSD::Notify("live_replay hook disabled!", Color::LimeGreen);
+			CTRPluginFramework::OSD::Notify("live_replay hook disabled!", Color::LimeGreen);
 		}
 		else
 		{
-			OSD::Notify("live_replay hook failed to disable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("live_replay hook failed to disable!", Color::Red);
 		}
 	}
 }
@@ -267,9 +266,9 @@ void replay_ghost_old(MenuEntry* entry)
 	u32 some_int = 0;
 	u32 int_0x208 = 0;
 
-	if (is_racing() && Process::Read32(0x0FFFF5D4, data) && Process::Read32(data - 0x00000004, data) && Process::Read32(data + 0x00000018, data))
+	if (is_racing() && CTRPluginFramework::Process::Read32(0x0FFFF5D4, data) && CTRPluginFramework::Process::Read32(data - 0x00000004, data) && CTRPluginFramework::Process::Read32(data + 0x00000018, data))
 	{
-		if (Process::Read32(0x14000084, offset) && Process::Read32(offset + 0x0000316C, offset) && FCRAM_0_RANGE(offset))
+		if (CTRPluginFramework::Process::Read32(0x14000084, offset) && CTRPluginFramework::Process::Read32(offset + 0x0000316C, offset) && FCRAM_0_RANGE(offset))
 		{
 			//Process::Read8(offset + 0x00000119, char_0x119);
 			//Process::Write8(offset + 0x00000119, 0);
@@ -277,20 +276,20 @@ void replay_ghost_old(MenuEntry* entry)
 			/*
 				target camera and model to ghost instead of player
 			*/
-			Process::Read32(data + 0x00000208, int_0x208);
-			Process::Write32(data + 0x00000208, 0x00010001);
+			CTRPluginFramework::Process::Read32(data + 0x00000208, int_0x208);
+			CTRPluginFramework::Process::Write32(data + 0x00000208, 0x00010001);
 
-			if (Process::Read32(0x0065DA44, data) && Process::Read32(data + 0x000020E0, data))
+			if (CTRPluginFramework::Process::Read32(0x0065DA44, data) && CTRPluginFramework::Process::Read32(data + 0x000020E0, data))
 			{
 				// Move player to above finish line
-				Process::Read32(data + 0x24, some_int);
-				Process::Write32(data + 0x24, 0x49000000);
+				CTRPluginFramework::Process::Read32(data + 0x24, some_int);
+				CTRPluginFramework::Process::Write32(data + 0x24, 0x49000000);
 			}
 		}
 	}
 }
 
-void ghost_disable_overwrite(MenuEntry* entry)
+void ghost_disable_overwrite(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
@@ -299,7 +298,7 @@ void ghost_disable_overwrite(MenuEntry* entry)
 			0xeb000061, 0xe320f000, 0xe320f000, 0xea000008, 0xe1a01004
 		};
 
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00400000, func_pattern);
 
 		if (func_ptr)
 		{
@@ -307,11 +306,11 @@ void ghost_disable_overwrite(MenuEntry* entry)
 
 			// System::SaveDataManager::calcInner_(int) + 0xa8
 			// disable writing new ghosts by replacing instruction with an NOP
-			Process::Write32(func_ptr, 0xe320f000);
+			CTRPluginFramework::Process::Write32(func_ptr, 0xe320f000);
 		}
 		else
 		{
-			OSD::Notify("never save ghosts failed to enable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("never save ghosts failed to enable!", CTRPluginFramework::Color::Red);
 			entry->Disable();
 		}
 	}
@@ -323,17 +322,17 @@ void ghost_disable_overwrite(MenuEntry* entry)
 			0xe320f000, 0xe320f000, 0xe320f000, 0xea000008, 0xe1a01004
 		};
 
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
 
 		svcInvalidateEntireInstructionCache();
 
 		// restore old instruction
-		Process::Write32(func_ptr, 0xeb000061);
+		CTRPluginFramework::Process::Write32(func_ptr, 0xeb000061);
 	}
 }
 
 
-void ghost_hide(MenuEntry* entry)
+void ghost_hide(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
@@ -343,82 +342,82 @@ void ghost_hide(MenuEntry* entry)
 		};
 
 		// search through part of exefs memory for our instruction ptr
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
 
 		overwrite_ghost_alpha_hook.Initialize(func_ptr + 0x20, reinterpret_cast<u32>(change_ghost_alpha));
 
 		// dont run the overwritten instruction
 		overwrite_ghost_alpha_hook.SetFlags(USE_LR_TO_RETURN);
 
-		if (overwrite_ghost_alpha_hook.Enable() == HookResult::Success)
+		if (overwrite_ghost_alpha_hook.Enable() == CTRPluginFramework::HookResult::Success)
 		{
-			OSD::Notify("overwrite_ghost_alpha hook enabled!", Color::LimeGreen);
+			CTRPluginFramework::OSD::Notify("overwrite_ghost_alpha hook enabled!", CTRPluginFramework::Color::LimeGreen);
 		}
 		else
 		{
-			OSD::Notify("overwrite_ghost_alpha hook failed to enable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("overwrite_ghost_alpha hook failed to enable!", CTRPluginFramework::Color::Red);
 			overwrite_ghost_alpha_hook.Disable();
 		}
 	}
 
 	if (!entry->IsActivated())
 	{
-		if (overwrite_ghost_alpha_hook.Disable() == HookResult::Success)
+		if (overwrite_ghost_alpha_hook.Disable() == CTRPluginFramework::HookResult::Success)
 		{
-			OSD::Notify("overwrite_ghost_alpha hook disabled!", Color::LimeGreen);
+			CTRPluginFramework::OSD::Notify("overwrite_ghost_alpha hook disabled!", CTRPluginFramework::Color::LimeGreen);
 		}
 		else
 		{
-			OSD::Notify("overwrite_ghost_alpha hook failed to disable!", Color::Red);
+			CTRPluginFramework::OSD::Notify("overwrite_ghost_alpha hook failed to disable!", CTRPluginFramework::Color::Red);
 		}
 	}
 }
 
-void disable_music(MenuEntry* entry)
+void disable_music(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
- 		const std::vector <u32> func_pattern =
+ 		const std::vector<u32> func_pattern =
  		{
 			0xEBFFFFA5, 0xE3500000, 0x1A000002, 0xE594001C, 0xE3A01001, 0xE5C0108A
  		};
  
- 		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
+ 		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00800000, func_pattern);
 
 		svcInvalidateEntireInstructionCache();
 
 		// disables course ambient audio
 		//Process::Write32(0x003DA0CC, 0xe320f000);
 
-		Process::Write32(func_ptr, 0xe320f000);
+		CTRPluginFramework::Process::Write32(func_ptr, 0xe320f000);
 	}
 
 	if (!entry->IsActivated())
 	{
-		const std::vector <u32> func_pattern =
+		const std::vector<u32> func_pattern =
 		{
 			0xe320f000, 0xE3500000, 0x1A000002, 0xE594001C, 0xE3A01001, 0xE5C0108A
 		};
 
-		u32 func_ptr = Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
+		u32 func_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00300000, func_pattern);
 
 		svcInvalidateEntireInstructionCache();
 
 		// restore original function (BL nw__snd__internal__driver__StreamSoundPlayer__StreamDataLoadTask__LoadStreamData)
-		Process::Write32(func_ptr, 0xebffffa5);
+		CTRPluginFramework::Process::Write32(func_ptr, 0xebffffa5);
 	}
 }
 
-void new_aspect_ratio(MenuEntry* entry)
+void new_aspect_ratio(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
-		const std::vector <u32> memory_pattern =
+		const std::vector<u32> memory_pattern =
 		{
 			0xebfa0830, 0xe5c47102, 0xecbd8b02, 0xe8bd81f0
 		};
 
-		u32 mem_ptr = Utils::Search<u32>(0x00100000, 0x00400000, memory_pattern);
+		u32 mem_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00800000, memory_pattern);
 		
 		if (mem_ptr)
 		{
@@ -426,7 +425,7 @@ void new_aspect_ratio(MenuEntry* entry)
 		}
 		else
 		{
-			OSD::Notify("aspect ratio var not found!", Color::White, Color::Maroon);
+			CTRPluginFramework::OSD::Notify("aspect ratio var not found!", CTRPluginFramework::Color::White, CTRPluginFramework::Color::Maroon);
 			entry->Disable();
 			return;
 		}
@@ -443,29 +442,29 @@ void new_aspect_ratio(MenuEntry* entry)
 			"21:9"
 		};
 
-		Keyboard aspect_ratio_option("Select aspect ratio", strings);
+		CTRPluginFramework::Keyboard aspect_ratio_option("Select aspect ratio", strings);
 		u32 selection = aspect_ratio_option.Open();
 
 		switch (selection)
 		{
 		case 0:
-			Process::Write32(mem_ptr, 0x3fd55555); // 16:10 (Original)
+			CTRPluginFramework::Process::Write32(mem_ptr, 0x3fd55555); // 16:10 (Original)
 			//Process::Write32(mem_ptr + 4, 0x3fd55555); // 16:10 (Original)
 			break;
 		case 1:
-			Process::Write32(mem_ptr, 0x3faaaaab); // 4:3
+			CTRPluginFramework::Process::Write32(mem_ptr, 0x3faaaaab); // 4:3
 			//Process::Write32(mem_ptr + 4, 0x3faaaaab); // 4:3
 			break;
 		case 2:
-			Process::Write32(mem_ptr, 0x3fe38e39); // 16:9
+			CTRPluginFramework::Process::Write32(mem_ptr, 0x3fe38e39); // 16:9
 			//Process::Write32(mem_ptr + 4, 0x3fe38e39); // 16:9
 			break;
 		case 3:
-			Process::Write32(mem_ptr, 0x40155555); // 21:9
+			CTRPluginFramework::Process::Write32(mem_ptr, 0x40155555); // 21:9
 			//Process::Write32(mem_ptr + 4, 0x40155555); // 21:9
 			break;
 		default:
-			OSD::Notify("invalid option!", Color::White, Color::Maroon);
+			CTRPluginFramework::OSD::Notify("invalid option!", CTRPluginFramework::Color::White, CTRPluginFramework::Color::Maroon);
 			entry->Disable();
 			break;
 		}
@@ -473,20 +472,20 @@ void new_aspect_ratio(MenuEntry* entry)
 
 	if (!entry->IsActivated())
 	{
-		const std::vector <u32> memory_pattern =
+		const std::vector<u32> memory_pattern =
 		{
 			0xebfa0830, 0xe5c47102, 0xecbd8b02, 0xe8bd81f0
 		};
 
-		u32 mem_ptr = Utils::Search<u32>(0x00100000, 0x00400000, memory_pattern);
+		u32 mem_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00800000, memory_pattern);
 
 		if (mem_ptr)
 		{
-			OSD::Notify(Utils::Format("%p", mem_ptr));
+			CTRPluginFramework::OSD::Notify(CTRPluginFramework::Utils::Format("%p", mem_ptr));
 		}
 		else
 		{
-			OSD::Notify("aspect ratio var not found!");
+			CTRPluginFramework::OSD::Notify("aspect ratio var not found!");
 			return;
 		}
 
@@ -498,33 +497,47 @@ void new_aspect_ratio(MenuEntry* entry)
 	}
 }
 
-void test_popup(MenuEntry* entry)
+/*
+void test_popup(CTRPluginFramework::MenuEntry* entry)
 {
 	if (entry->WasJustActivated())
 	{
-		// first 6 instructions of UI::UnitTextPane::create(nw::lyt::Pane *, wchar_t *, int, bool)
-		const std::vector <u32> memory_pattern =
+		const std::vector<u32> g_root_system_pattern =
 		{
-			0xe92d4fff, 0xe1a04000, 0xe1b05001, 0xe1a0b001, 0xe1a0b001, 0xe59f64a0
+			0xe5952000, 0xe8bd4070
 		};
 
-		u32 mem_ptr = Utils::Search<u32>(0x00100000, 0x00400000, memory_pattern);
+		System::g_root_system = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00800000, memory_pattern);
+
+		if (System::g_root_system)
+		{
+			OSD::Notify("RootSystem ptr: %p", System::g_root_system);
+		}
+
+		// first 6 instructions of UI::UnitTextPane::create(nw::lyt::Pane *, wchar_t *, int, bool)
+		const std::vector<u32> memory_pattern =
+		{
+			0xe92d4fff, 0xe1a04000, 0xe1b05001, 0xe1a0b001, 0xe59f64a0, 0xed2d8b02
+		};
+
+		u32 mem_ptr = CTRPluginFramework::Utils::Search<u32>(0x00100000, 0x00800000, memory_pattern);
 
 		if (mem_ptr)
 		{
-			//OSD::Notify(Utils::Format("%p", mem_ptr));
+			CTRPluginFramework::OSD::Notify(CTRPluginFramework::Utils::Format("%p", mem_ptr));
 		}
 		else
 		{
-			OSD::Notify("aspect ratio var not found!", Color::White, Color::Maroon);
+			CTRPluginFramework::OSD::Notify("UI::UnitTextPane::create() var not found!", CTRPluginFramework::Color::White, CTRPluginFramework::Color::Maroon);
 			entry->Disable();
 			return;
 		}
 
-		nw::lyt::Pane new_pane;
-		const wchar_t* text = L"hello!";
-		UI::UnitTextPane::create(&new_pane, text, 0, 0, false);
+		//nw::lyt::Pane* new_pane = new nw::lyt::Pane;
+		//const wchar_t* text = L"hello!";
+		//UI::UnitTextPane::create(new_pane, text, 0, 0, false);
 
 		entry->Disable();
 	}
 }
+*/
